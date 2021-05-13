d78cb7fa6956e4d834484b87de338a49b9ca5de2
Tack7 <tack.jpn1@gmail.com>
2021-05-13 22:43:54 +0900

    ユーザーAPI

--- backend/go.mod
+++ backend/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-playground/validator/v10 v10.6.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,75 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "g2538Rk0"
+	PROTOCOL := "tcp(localhost:3306)"
+	DBNAME := "team_4"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("db connected: ", &db)
+	return db
+}
 
 func main() {
+	db := gormConnect()
+
+	defer db.Close()
+	db.LogMode(true)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello world")
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	//CRUDのREST API を作成
+	r.POST("/user", func(c *gin.Context) {
+		user := User{}
+
+		err := c.BindJSON(&user)
+		if err != nil {
+			c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+		}
+		db.NewRecord(user)
+		db.Create(&user)
+		if db.NewRecord(user) == false {
+			c.JSON(http.StatusOK, user)
+		}
+	})
+	//READ
+	//全レコード
+	r.GET("/users", func(c *gin.Context) {
+		users := []User{}
+		db.Find(&users) // 全レコード
+		c.JSON(http.StatusOK, users)
+	})
+	//1レコード
+	r.GET("/user/:id", func(c *gin.Context) {
+		user := User{}
+		id := c.Param("id")
+
+		db.Where("ID = ?", id).First(&user)
+		c.JSON(http.StatusOK, user)
+	})
+
+	r.Run(":8080")
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Id       int    `json:"id"`
+	Password string `json:"password"`
 }
