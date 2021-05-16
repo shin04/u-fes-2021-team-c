4057fab23aa62ee4a3e7dbf2e7cff228d056c512
shin04 <daikon0413@gmail.com>
2021-05-16 22:44:02 +0900

    implement userAPI

--- backend/database/usesr.go
+++ backend/database/usesr.go
@@ -14,3 +14,23 @@ func (repo *UserRepository) CreateUser(user *model.User) (int, error) {
 
 	return user.Id, nil
 }
+
+func (repo *UserRepository) GetAllUsers() ([]*model.User, error) {
+	users := []*model.User{}
+	result := repo.SqlHandler.Conn.Find(&users)
+	if err := result.Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (repo *UserRepository) GetUserById(userId int) (*model.User, error) {
+	user := &model.User{}
+	result := repo.SqlHandler.Conn.Where("ID = ?", userId).First(&user)
+	if err := result.Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
