715b27ed1fceb385386a244519663559a889dbed
agriculture66 <daigorin-www@ezweb.ne.jp>
2021-06-03 17:16:18 +0900

    ページ作成

--- frontend/src/Sample.js
+++ frontend/src/Sample.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Sample() {
-  return <p>畑中大吾</p>;
+  constructor(props) {
+    super(props);
+    this.state = {name:"ファイル形式"};
+    this.state = {count: 0};
+  }
+handleClick(){
+  this.setState({count:this.state.count +1});
+}
+handleClick(name){
+  this.setState({name:name});
+}
+  render() {
+    return (
+      <div>
+         <h1>ファイルを{this.state.name}に変更</h1>
+         <button onClick={() => {this.setState({name:'PDF'})}}>PDF</button>
+         <button onClick={() => {this.setState({name:'JPEG'})}}>JPEG</button>
+        <button onClick={()=>{this.handleClick()}}>+</button>
+      </div>
+    );
+  }
 }
 
 export default Sample;
