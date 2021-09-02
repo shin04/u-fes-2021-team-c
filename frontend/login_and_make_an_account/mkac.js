fa4a2c4120086e43005cbc80ca6cc006f5f3e982
mattsunkun <mattsunkun1221@gmail.com>
2021-09-02 19:51:11 +0900

    ダイシンさん，確認おねがいします．

---
+++ frontend/login_and_make_an_account/mkac.js
@@ -0,0 +1,45 @@
+/* function green_tea() {
+    const fetchForm = document.querySelector('.form');
+const data = new FormData(fetchForm);
+const param  = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data)
+};
+fetch("https://nufes-teamc.herokuapp.com/user", param)
+  .then((res)=>{
+    return( res.json() );
+  })
+  .then((json)=>{
+    console.log(json);
+});
+}
+const btn = document.querySelector('.button');
+btn.addEventListener('click', green_tea, false); 
+*/
+
+const mkac = () {
+  const username = document.mkac_form.name.value;
+  const password = document.mkac_form.password.value;
+
+  const data = { 
+    name: username, 
+    password: password, 
+  };
+  const param = {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json', 
+    }, 
+    body: JSON.stringify(data), 
+  };
+  fetch('https://nufes-teamc.herokuapp.com/mkac', param)
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
+};
