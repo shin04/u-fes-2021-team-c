7211dee4ffc8b7d4368bb4934f2f85835cd4a408
shin04 <daikon0413@gmail.com>
2021-09-02 13:40:56 +0900

    fix login process

--- frontend/login_and_make_an_account/login.js
+++ frontend/login_and_make_an_account/login.js
@@ -1,39 +1,60 @@
 function hello() {
-    console.log('hello!');
-  }
+  console.log("hello!");
+}
 function ocha() {
-    fetch("https://nufes-teamc.herokuapp.com/users")
-  .then((res)=>{
-    return( res.json() );
-  })
-  .then((json)=>{
-    console.log(json);
-  });
+  fetch("https://nufes-teamc.herokuapp.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+    });
 }
 
+const fetchForm = document.querySelector("login_form");
+const btn = document.querySelector(".button");
 
+// function green_tea() {
+//   const data = new FormData(fetchForm);
+//   console.log(data);
+//   console.log(JSON.stringify(data));
+//   const param = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   };
+//   fetch("https://nufes-teamc.herokuapp.com/login", param)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//     });
+// }
+// btn.addEventListener("click", green_tea, false);
 
+const login = () => {
+  // username = document.login_form.name.value;
+  // password = document.login_form.password.value;
 
-
-
-function green_tea() {
-    const fetchForm = document.querySelector('form');
-const data = new FormData(fetchForm);
-console.log(JSON.stringify(data))
-const param  = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(data)
+  const data = {
+    name: username,
+    password: password,
+  };
+  const param = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  fetch("https://nufes-teamc.herokuapp.com/login", param)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+    });
 };
-fetch("https://nufes-teamc.herokuapp.com/login", param)
-  .then((res)=>{
-    return( res.json() );
-  })
-  .then((json)=>{
-    console.log(json);
-});
-}
-const btn = document.querySelector('.button');
-btn.addEventListener('click', green_tea, false);
