bd046d23405a61e439480f3448cdc7f5cc6b89e0
mattsunkun <mattsunkun1221@gmail.com>
2021-09-10 13:47:48 +0900

    正規表現で不正な入力及び未入力を弾くようにしました．

--- frontend/login-mkac/login.js
+++ frontend/login-mkac/login.js
@@ -1,31 +1,36 @@
 const login = () => {
   const username = document.login_form.name.value;
   const password = document.login_form.password.value;
+  const username_bool = (username == username.match(/\w+/g));
+  const password_bool = (password == password.match(/\w+/g));
+  if (username_bool&&passwordbool) {
+    const data = {
+      name: username,
+      password: password,
+    };
+    const param = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("https://nufes-teamc.herokuapp.com/login", param)
+      .then((res) => {
+        if( ! res.ok ) {
+          throw new Error(`Fetch: ${res.status} ${res.statusText}`);
+        }
+        return( res.json() );
+      })
+      .then((json) => {
+        console.log(json);
+        window.location.href = './mkpdf.html';
 
-  const data = {
-    name: username,
-    password: password,
+      })
+      .catch((error)=>{
+        alert('エラー');
+      });
+  }else{
+    console.error(username, password);
   };
-  const param = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-  fetch("https://nufes-teamc.herokuapp.com/login", param)
-    .then((res) => {
-      if( ! res.ok ) {
-        throw new Error(`Fetch: ${res.status} ${res.statusText}`);
-      }
-      return( res.json() );
-    })
-    .then((json) => {
-      console.log(json);
-      window.location.href = './mkpdf.html';
-
-    })
-    .catch((error)=>{
-      alert('エラー');
-    });
 };
